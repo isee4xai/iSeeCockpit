129b38632c277d56045143d8be16f77f0dd6f8a7
xernois <antoine2496@gmail.com>
2022-04-28 19:41:51 +0200

    [feat] drag and drop options for the Checkbox input

--- src/components/iSee/question/toolkit/CheckboxInput.tsx
+++ src/components/iSee/question/toolkit/CheckboxInput.tsx
@@ -1,7 +1,9 @@
 import './CheckboxInput.less';
 import { useEffect, useState } from 'react';
 import { Checkbox, Space, Input } from 'antd';
-import { PlusSquareOutlined } from '@ant-design/icons';
+import { PlusSquareOutlined, HolderOutlined } from '@ant-design/icons';
+import Draggable from 'react-draggable';
+import useDrag from './useDrag';
 
 const CheckboxInput: React.FC<{
   options?: string[];
@@ -10,7 +12,9 @@ const CheckboxInput: React.FC<{
   const [add, setAdd] = useState<boolean>(false);
   const [edit, setEdit] = useState<number>(-1);
 
-  const [optionsList, setOptionList] = useState(options || []);
+  const [optionsList, handleStartDrag, handleDrag, handleStopDrag, setOptionList] = useDrag(
+    options || [],
+  );
 
   const handleAdd = () => setAdd(true);
   const handleEdit = (idx: number) => setEdit(idx);
@@ -37,20 +41,34 @@ const CheckboxInput: React.FC<{
     <>
       <Checkbox.Group value={undefined}>
         <Space direction="vertical" size={0}>
-          {optionsList.map((option, idx) => (
-            <div key={idx} className="container" drag-index={idx} style={{ padding: '4px 0' }}>
-              <Checkbox className="checkbox-item" value={option} onClick={() => handleEdit(idx)}>
-                {edit === idx ? (
-                  <Input
-                    defaultValue={option}
-                    onBlur={(event) => handleEditComplete(idx, event.target.value)}
-                    autoFocus
-                  />
-                ) : (
-                  option
-                )}
-              </Checkbox>
-            </div>
+          {optionsList.map((option, idx, tab) => (
+            <Draggable
+              axis="y"
+              bounds={{ top: -5 - 38 * idx, bottom: 5 + (tab.length - idx - 1) * 38 }}
+              key={`${idx}-${option}-${Date.now()}`}
+              onStart={handleStartDrag}
+              onStop={handleStopDrag}
+              onDrag={(event) => {
+                setEdit(-1);
+                handleDrag(event);
+              }}
+              handle={'.checkbox-holder'}
+            >
+              <div key={idx} className="container" drag-index={idx} style={{ padding: '4px 0' }}>
+                <HolderOutlined className="checkbox-holder" />
+                <Checkbox className="checkbox-item" value={option} onClick={() => handleEdit(idx)}>
+                  {edit === idx ? (
+                    <Input
+                      defaultValue={option}
+                      onBlur={(event) => handleEditComplete(idx, event.target.value)}
+                      autoFocus
+                    />
+                  ) : (
+                    option
+                  )}
+                </Checkbox>
+              </div>
+            </Draggable>
           ))}
         </Space>
       </Checkbox.Group>
