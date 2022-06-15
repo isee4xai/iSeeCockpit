706357e6b0b23d06cf1565ad394366e5c06c1280
deliasTheo <t22052002>
2022-06-15 16:43:56 +0100

    export csv

--- src/components/iSee/analytics/IntentAnalytics.tsx
+++ src/components/iSee/analytics/IntentAnalytics.tsx
@@ -1,8 +1,9 @@
-import { Card, Col, Descriptions, Row } from 'antd';
+import { Button, Card, Col, Descriptions, Modal, notification, Row } from 'antd';
 
 import DataVisualizer from '@/components/iSee/analytics/DataVisualizer';
 
 import { api_intent_stat } from '@/services/isee/usecases';
+import { CopyOutlined } from '@ant-design/icons';
 import Meta from 'antd/lib/card/Meta';
 import { useEffect, useState } from 'react';
 
@@ -22,6 +23,83 @@ const IntentAnalytics: React.FC<{
     })();
   }, [intent, persona, usecase]);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [currentDiagram, setcurrentDiagram] = useState([]);
+  let csv = '';
+
+  const showModal = (jsonData: any) => {
+    setcurrentDiagram(jsonData);
+    setIsModalVisible(true);
+  };
+
+  const generateModal = () => {
+    const handleOk = () => {
+      setIsModalVisible(false);
+    };
+    const handleCloseModal = () => {
+      setIsModalVisible(false);
+    };
+    if (currentDiagram != undefined) {
+      for (let index = 0; index < currentDiagram.length + 1; index++) {
+        if (index == 0) {
+          csv += Object.keys(currentDiagram[0]).map((element) => {
+            return element;
+          });
+        } else {
+          csv +=
+            '\r\n' +
+            currentDiagram[index - 1][`${Object.keys(currentDiagram[0])[0]}`] +
+            ', ' +
+            currentDiagram[index - 1][`${Object.keys(currentDiagram[0])[1]}`];
+        }
+      }
+    }
+    const addToClipboard = () => {
+      navigator.clipboard.writeText(csv).then(
+        () => {
+          notification.success({
+            message: `Success`,
+            duration: 3,
+            description: 'The questionnaire json has been added to the clipboard',
+            placement: 'bottomRight',
+          });
+        },
+        () => {
+          notification.error({
+            message: `Error`,
+            duration: 3,
+            description: 'An error occured, the json was not added to the clipboard',
+            placement: 'bottomRight',
+          });
+        },
+      );
+    };
+
+    return (
+      <Modal
+        title={'Export'}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCloseModal}
+        footer={
+          <>
+            {
+              <Button onClick={addToClipboard} icon={<CopyOutlined />}>
+                Copy to clipboard
+              </Button>
+            }
+
+            <Button type="primary" onClick={handleOk}>
+              Close
+            </Button>
+          </>
+        }
+      >
+        <pre>{<code onClick={(e) => console.dir(e.target)}>{csv}</code>}</pre>
+      </Modal>
+    );
+  };
+
   return (
     <Card>
       <Row gutter={20}>
@@ -34,7 +112,10 @@ const IntentAnalytics: React.FC<{
             md={{ order: 3 }}
             lg={{ order: 4 }}
           >
-            <Card title={'Question ' + (index + 1)}>
+            <Card
+              title={'Question ' + (index + 1)}
+              extra={<Button onClick={() => showModal(item.values)}>Export CSV</Button>}
+            >
               <Descriptions.Item label={item.question}>{item.question}</Descriptions.Item>
               <Meta description={'(' + item.dimension + ')'} />
               <DataVisualizer
@@ -48,6 +129,7 @@ const IntentAnalytics: React.FC<{
           </Col>
         ))}
       </Row>
+      {generateModal}
       {/* <Form
                 name="basic"
                 labelCol={{ span: 8 }}
