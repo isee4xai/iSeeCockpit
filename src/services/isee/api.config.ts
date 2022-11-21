8243caa49f89a2d735cbaa3b7b98ca69fb37314d
Chamath Palihawadana <chamathpali123@gmail.com>
2022-11-21 18:57:22 +0100

    revereted to reactapp for env variables

--- src/services/isee/api.config.ts
+++ src/services/isee/api.config.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = process.env.ISEE_API || 'http://localhost:3000/api';
-export const ONTOAPI_URL = process.env.ISEE_ONTOAPI || 'http://localhost:3100/api';
+
+export const BASE_URL = process.env.REACT_APP_ISEE_API || 'http://localhost:3000/api';
+export const ONTOAPI_URL = process.env.REACT_APP_ISEE_ONTOAPI || 'http://localhost:3100/api';
