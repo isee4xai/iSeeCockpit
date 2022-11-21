bb0e6f2d8b2f1bd89245fffaead3eca1b98917c3
anjanaw <shyameniw@gmail.com>
2022-11-21 20:09:09 +0100

    explainer UI

---
+++ src/models/explainer.ts
@@ -0,0 +1,19 @@
+export interface Explainer {
+    _id?: string;
+    name: string;
+    explainer_description?: string;
+    technique?: string;
+    dataset_type?: string;
+    explanation_type?: string;
+    explanation_description?: string;
+    concurrentness?: string;
+    scope?: string;
+    portability?: string;
+    target?: string;
+    presentations?: string[];
+    complexity?: string;
+    ai_methods?: string[];
+    ai_tasks?: string[];
+    implementation?: string;
+    metadata?: string;
+}
