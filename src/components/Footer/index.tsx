e1571c6caa8546e5fc50424179265912d5cdee15
Chamath Palihawadana <chamathpali123@gmail.com>
2022-11-22 01:25:47 +0100

    Onto API Integration for Explainer Dashboard

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import { DefaultFooter } from '@ant-design/pro-layout';
 
 const Footer: React.FC = () => {
-  const defaultMessage = 'iSee: Building the AI you trust - v1.007';
+  const defaultMessage = 'iSee: Building the AI you trust - v1.008';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
