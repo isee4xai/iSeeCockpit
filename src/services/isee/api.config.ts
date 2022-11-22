dac3008d936327a2a1e4d6fb4038d20f74b18e8c
Chamath Palihawadana <chamathpali123@gmail.com>
2022-11-22 16:06:02 +0100

    Debug - ENV Variables

--- src/services/isee/api.config.ts
+++ src/services/isee/api.config.ts
@@ -1,3 +1,7 @@
+export const BASE_URL = process.env.NODE_ENV === 'production' ?
+    'https://api-dev.isee4xai.com/api'
+    : 'http://localhost:3000/api';
 
-export const BASE_URL = process.env.REACT_APP_ISEE_API || 'http://localhost:3000/api';
-export const ONTOAPI_URL = process.env.REACT_APP_ISEE_ONTOAPI || 'http://localhost:3100/api';
+export const ONTOAPI_URL = process.env.NODE_ENV === 'production'
+    ? 'https://api-onto-dev.isee4xai.com/api'
+    : 'http://localhost:3100/api';
