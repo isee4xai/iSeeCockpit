738edd4a68f4af9ccbc591c7a67f7ad2e05891cf
anjanaw <shyameniw@gmail.com>
2023-02-09 18:11:08 +0000

    chatbot updates

--- src/components/iSee/chatbot/AnswerRadio.tsx
+++ src/components/iSee/chatbot/AnswerRadio.tsx
@@ -1,24 +1,32 @@
 import { Radio, Space } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import { Response } from '@/models/questionnaire';
 import './Chatbot.less';
 
 
-const AnswerRadio = ({ listAnswer, onChange }) => {
+const AnswerRadio = ({ listAnswer, onChange, disabled, selected }) => {
   const [value, setValue] = React.useState<Response>();
 
   const isChanged = (e: any) => {
     setValue(e.target.value);
   };
 
+  useEffect(() => {
+    if (selected) {
+      setValue(selected);
+    }
+  }, []);
+
   return (
     <div>
-      <Radio.Group onChange={isChanged} value={value}>
+      <Radio.Group onChange={!disabled ? isChanged : undefined} value={value}>
         <Space size={10} direction="horizontal" wrap id="spaceRadio">
           {listAnswer.map((element: Response, idx: number) => (
             <Radio.Button
-              onChange={onChange}
+              onChange={!disabled ? onChange : undefined}
               value={element}
+              // disabled={disabled}
               key={'radio' + idx}
               className="radioButton"
               style={{ border: 0 }}
