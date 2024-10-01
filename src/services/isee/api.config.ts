f537a85f171b40996645c3de6ef8cc4df524b7af
bruno fleisch <bruno.fleisch@bt.com>
2024-10-01 14:37:41 +0200

    add support for LLM-enabled dialog manager + fix configuration with env variables

--- src/services/isee/api.config.ts
+++ src/services/isee/api.config.ts
@@ -1,13 +1,5 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ?
-    'https://api-dev.isee4xai.com/api'
-    : 'http://localhost:3000/api';
+export const BASE_URL    = process.env.UMI_APP_API_URL      || 'http://localhost:3000/api';
+export const ONTOAPI_URL = process.env.UMI_APP_ONTOAPI_URL  || 'http://localhost:3100/api';
+export const WS_URL      = process.env.UMI_APP_WS_URL       || 'ws://127.0.0.1:8000/ws/';
+export const EDITOR_URL  = process.env.UMI_APP_EDITOR_URL   ||  "https://editor-dev.isee4xai.com/";
 
-export const ONTOAPI_URL = process.env.NODE_ENV === 'production'
-    ? 'https://api-onto-dev.isee4xai.com/api'
-    : 'http://localhost:3100/api';
-
-export const WS_URL = process.env.NODE_ENV === 'production'
-    ? 'wss://dialog-dev.isee4xai.com/ws/'
-    : 'ws://127.0.0.1:8000/ws/';
-
-export const EDITOR_URL = "https://editor-dev.isee4xai.com/";
